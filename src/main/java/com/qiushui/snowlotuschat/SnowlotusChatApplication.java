19a7fa1273c6902d8e3f8d5d5f5fad8cdf903d02
qiushui <if_guo@163.com>
2019-07-13 17:48:22 +0800

    初始化项目

---
+++ src/main/java/com/qiushui/snowlotuschat/SnowlotusChatApplication.java
@@ -0,0 +1,13 @@
+package com.qiushui.snowlotuschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowlotusChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowlotusChatApplication.class, args);
+	}
+
+}
